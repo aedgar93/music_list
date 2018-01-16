aeb44a2ee20e9f658f00be653f636fa2271cf700
unknown <aedgar93@uw.edu>
2018-01-16 12:47:23 -0800

    Start on styling the music list. Update with correct image URLS

--- app/app.js
+++ app/app.js
@@ -12,4 +12,9 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
     templateUrl: 'views/browse.html'
   })
   .otherwise({redirectTo: '/browse'}); //Right now we only have /browse, so always redirect there
-}]);
+}]).
+filter('secondsToTime', ['$filter', function($filter) { //Convert seconds to m:ss
+  return function(seconds) {
+    return $filter('date')(new Date(0, 0, 0).setSeconds(seconds), 'm:ss');
+  };
+}])
